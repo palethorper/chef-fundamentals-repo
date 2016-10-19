1be39eff9802aa81b01ab696a1f3d8092739bb85
Robert Palethorpe <palethorper@gmail.com>
2016-10-19 14:45:57 +1100

    added apache ohai plugin

---
+++ cookbooks/apache/files/default/plugins/modules.rb
@@ -0,0 +1,12 @@
+Ohai.plugin(:Apache) do
+	provides 
+	"apache/modules"
+
+	collect_data(:default) do
+		apache Mash.new
+		apache[:modules] = shell_out("apachectl -t -D DUMP_MODULES").stdout
+		#modules = shell_out("apachectl -t -D DUMP_MODULES").stdout
+		#apache[:modules] = modules.stdout
+	end
+
+end
