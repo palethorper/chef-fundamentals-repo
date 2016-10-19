a4f61fee26d89bb9becae7c88068c6f155b48fe0
Robert Palethorpe <palethorper@gmail.com>
2016-10-19 15:14:51 +1100

    Added parsing of results for ohai apache plugin

--- cookbooks/apache/files/default/plugins/modules.rb
+++ cookbooks/apache/files/default/plugins/modules.rb
@@ -3,6 +3,21 @@ Ohai.plugin(:Apache) do
 
 	collect_data(:default) do
 		apache Mash.new
-		apache[:modules] = shell_out("apachectl -t -D DUMP_MODULES").stdout
+		apache[:modules] = { :static => [], :shared => [] }
+		modules = shell_out("apachectl -t -D DUMP_MODULES").stdout
+		
+		#apache[:modules] = modules.stdout
+		modules.stdout.each_line do |line|
+			fullkey, value = line.split("(", 2).map { |token| token.strip }
+			apache_module = fullkey.gsub("_module","")
+			dso_type = value.to_s.chomp("\)")
+			
+			if dso_type.match(/shared/)
+				apache[:modules][:shared] << apache_module
+			elsif dso_type.match(/static/)
+				apache[:modules][:static] << apache_module
+			end
+		end
 	end
+
 end
