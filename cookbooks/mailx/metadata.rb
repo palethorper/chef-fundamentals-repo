460d1832679f08f8e67ff564b5abdefbc5a72f82
Robert Palethorpe <palethorper@gmail.com>
2016-10-20 10:51:21 +1100

    Created mailx cookbook

---
+++ cookbooks/mailx/metadata.rb
@@ -0,0 +1,7 @@
+name             'mailx'
+maintainer       'Dimension Data'
+maintainer_email 'mark.greenwood@itaas.dimensiondata.com'
+license          'All rights reserved'
+description      'Installs/Configures mailx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ cookbooks/mailx/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: mailx
+# Recipe:: default
+#
+# Copyright 2016, Dimension Data
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "mailx" do
+	action :install
+end
+
