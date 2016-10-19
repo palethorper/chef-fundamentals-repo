2eed1640320618843c5d376ea9fbe7ad35fa91fc
Robert Palethorpe <palethorper@gmail.com>
2016-10-20 10:30:02 +1100

    created new cookbook email_handler

---
+++ cookbooks/email_handler/metadata.rb
@@ -0,0 +1,7 @@
+name             'email_handler'
+maintainer       'Dimension Data'
+maintainer_email 'Mark.Greenwood@itaas.dimensiondata.com'
+license          'All rights reserved'
+description      'Installs/Configures email_handler'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
