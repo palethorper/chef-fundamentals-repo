659778547b768bb50f0ce15e0bce26e4db6d298a
Robert Palethorpe <palethorper@gmail.com>
2016-10-19 15:19:10 +1100

    Removed stupid stdout that was swalling the array assignment

--- cookbooks/apache/files/default/plugins/modules.rb
+++ cookbooks/apache/files/default/plugins/modules.rb
@@ -4,14 +4,14 @@ Ohai.plugin(:Apache) do
 	collect_data(:default) do
 		apache Mash.new
 		apache[:modules] = { :static => [], :shared => [] }
-		modules = shell_out("apachectl -t -D DUMP_MODULES").stdout
+		modules = shell_out("apachectl -t -D DUMP_MODULES")
 		
 		#apache[:modules] = modules.stdout
 		modules.stdout.each_line do |line|
 			fullkey, value = line.split("(", 2).map { |token| token.strip }
 			apache_module = fullkey.gsub("_module","")
 			dso_type = value.to_s.chomp("\)")
-			
+
 			if dso_type.match(/shared/)
 				apache[:modules][:shared] << apache_module
 			elsif dso_type.match(/static/)
