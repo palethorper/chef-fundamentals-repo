6bb30378fcdae1a94e832a9548b3f04c2ed17b4a
Robert Palethorpe <palethorper@gmail.com>
2016-10-19 16:05:14 +1100

    Disable the password plugin for base role

--- roles/base.rb
+++ roles/base.rb
@@ -1,3 +1,9 @@
 name "base"
 description "Base Server Role"
 run_list "recipe[chef-client::config]", "recipe[chef-client::delete_validation]", "recipe[chef-client]", "recipe[ntp]", "recipe[motd]", "recipe[users]"
+
+default_attributes({
+	"ohai" => {
+	"disabled_plugins" => [ ":Passwd" ]
+	}
+})
