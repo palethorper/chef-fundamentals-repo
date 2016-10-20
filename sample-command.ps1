b90ba8adcecbbdede3d1813cddad97b622a2ec82
Robert Palethorpe <palethorper@gmail.com>
2016-10-20 11:48:43 +1100

    Updated sample ps commands

--- sample-command.ps1
+++ sample-command.ps1
@@ -4,7 +4,14 @@ knife client list
 knife cookbook upload -a
 knife cookbook upload apache
 
+knife upload data_bags
+
+knife role from file base.rb web.rb
+
 #Static code rule anaylsis
 foodcritic cookbooks/apache
 foodcritic cookbooks/apache -t ~FC003 -t ~FC009
 
+# Create new cook book
+# The "create version is going to be depricated"
+knife generate cookbook blah
